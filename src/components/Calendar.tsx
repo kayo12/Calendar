d41d56d0aff5e43b553040f4a0fbcaaaf827d905
Kayo campos <kayonbahenrique@gmail.com>
2022-08-11 23:40:29 -0300

    feat update: ajuste na requisição dos meses

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -2,33 +2,39 @@ import Mes from "./Mes";
 import { useEffect, useState, useSyncExternalStore } from "react";
 
 export default function Calendar(props) {
-
-  const [year, setYear] = useState("");
+  const [year, setYear] = useState(2022);
 
   useEffect(() => {
     OnChangeYear();
   }, [year]);
 
   function OnChangeYear() {
-    let ye = (document.getElementById("year") as HTMLInputElement).value;
-    console.log(ye);
-    return setYear(ye);
+    try {
+      let ye = (document.getElementById("year") as HTMLInputElement).value;
+      if (ye !== "") {
+        setYear(Number(ye));
+      }
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   return (
     <div className="calendar">
-      
       <div className="inputYear">
-        <input type="text" id="year" className="year" />
-        <button className="btnSearch"onClick={OnChangeYear} placeholder="Ex: 2025">Buscar</button>
+        <input type="text" id="year" className="year" required />
+        <button
+          className="btnSearch"
+          onClick={OnChangeYear}
+          placeholder="Ex: 2025"
+        >
+          Buscar
+        </button>
       </div>
       <div className="displayyear">
         <label htmlFor="year"> {year}</label>
       </div>
-      <Mes ano={Number(year)} />
+      <Mes anos={year} />
     </div>
   );
-
-
-  
 }
